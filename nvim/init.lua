4c96130b785d98d59ffd822bdc23c20e76cdcb37
Trard <trardone@gmail.com>
2022-10-12 16:39:11 +0300

    added nvim

---
+++ nvim/init.lua
@@ -0,0 +1,55 @@
+local fn = vim.fn
+
+-- Helpers
+require('helpers')
+
+-- General settings
+require('general')
+
+-- Packages
+local packer = require 'packer'
+packer.init {
+	compile_path = fn.stdpath('data') .. '/packer_compiled.lua'
+}
+
+local use = packer.use
+
+-- Packer itself
+use 'wbthomason/packer.nvim'
+
+-- Custom syntaxes
+use 'rust-lang/rust.vim'
+
+-- Autocomplete
+use {
+    'hrsh7th/nvim-cmp',
+    config = function() require('cmp_conf') end,
+    requires = {
+        'hrsh7th/vim-vsnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-calc',
+        'hrsh7th/cmp-nvim-lua',
+        'kdheepak/cmp-latex-symbols',
+    }
+}
+
+-- LSP
+use { 'neovim/nvim-lspconfig', config = function() require('lsp_conf') end }
+
+-- Pretty LSP diagnostics
+use {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+        require("lsp_lines").setup()
+    end,
+}
+
+-- Deep Ocean Theme
+use 'marko-cerovac/material.nvim'
+vim.g.material_style = "deep ocean"
+vim.cmd 'colorscheme material'
+
+-- Run all config
+dofile(fn.stdpath("data") .. "/packer_compiled.lua")
---
+++ nvim/lua/general.lua
@@ -0,0 +1,40 @@
+local o = vim.o
+
+-- Numbers
+o.number = true
+o.relativenumber = true
+
+-- Tabs
+o.expandtab = true
+o.smartindent = true
+o.smarttab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4       
+
+-- Cursor fix after exit
+vim.cmd
+[[
+    augroup change_cursor
+        au!
+        au ExitPre * :set guicursor=a:ver90
+    augroup END
+]]
+
+-- Some WSL patches
+if vim.fn.has("wsl") then
+    -- Allow copy
+    vim.g.clipboard = {
+        name = "clip.exe (Copy Only)",
+        copy = {
+            ["+"] = "clip.exe",
+            ["*"] = "clip.exe"
+        },
+        paste = {
+            ["+"] = "clip.exe",
+            ["*"] = "clip.exe"
+        },
+        cache_enabled = true
+    }
+end
+
