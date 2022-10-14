8ec2497c624c83e655d91c069357f993280b2aa3
Trard <trardone@gmail.com>
2022-10-14 14:15:10 +0300

    format

--- .config/nvim/lua/treesitter_conf.lua
+++ .config/nvim/lua/treesitter_conf.lua
@@ -1,19 +1,19 @@
-require("nvim-treesitter.configs").setup {
-    ensure_installed = { "c", "cpp", "rust", "lua", "python", "javascript", "json", "toml" },
+require("nvim-treesitter.configs").setup({
+	ensure_installed = { "c", "cpp", "rust", "lua", "python", "javascript", "json", "toml" },
 
-    auto_install = true, 
-    
-    highlight = {
-        enable = true,
-        disable = { "javascript" },
-    },
-    
-    rainbow = {
-        enable = true,
-        extended_mode = true,
+	auto_install = true,
 
-        colors = {
-            "#89DDFF"
-        }
-    }
-}
+	highlight = {
+		enable = true,
+		disable = { "javascript" },
+	},
+
+	rainbow = {
+		enable = true,
+		extended_mode = true,
+
+		colors = {
+			"#89DDFF",
+		},
+	},
+})
