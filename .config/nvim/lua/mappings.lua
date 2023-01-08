74be1d349d8e926f1c7c7001ac98d50da1c42cfe
Trard <trardone@gmail.com>
2023-01-08 20:19:17 +0300

    added mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,9 +2,15 @@ local map = vim.keymap.set
 
 vim.g.mapleader = ","
 
+-- Open the current directory
+map("n", "<leader>d", vim.cmd.Ex)
+
+-- Move visual block
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Exit insert mode with `jk`
 map("i", "jk", "<Esc>")
--- Do the same in terminal mode
 map("t", "jk", "<C-\\><C-N>")
 
 -- Format
@@ -13,6 +19,9 @@ map("n", "F", vim.cmd.Format)
 -- Rename
 map("n", "rn", vim.lsp.buf.rename, { noremap = true })
 
+-- Fast replace
+map("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 -- Run tests
 map("n", "tn", vim.cmd.TestNearest)
 map("n", "tf", vim.cmd.TestFile)
@@ -20,3 +29,6 @@ map("n", "tl", vim.cmd.TestLast)
 
 -- Undotree
 map("n", "<C-u>", vim.cmd.UndotreeToggle)
+
+-- Fix pasting
+map("x", "<leader>p", '"_dP')
