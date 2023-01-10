c027ff46b56bb20c03dfca2b8d039b041b25a339
Trard <trardone@gmail.com>
2023-01-10 14:26:09 +0300

    format

--- .config/nvim/lua/vim_diagnostics.lua
+++ .config/nvim/lua/vim_diagnostics.lua
@@ -1,5 +1,5 @@
 vim.diagnostic.config({
-  virtual_text = false,
+	virtual_text = false,
 })
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
